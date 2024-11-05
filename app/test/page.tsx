67d62df00a38c011b003a8e2b05431722e9756bd
fenghang <hang.feng@nx-engine.com>
2024-11-05 16:21:01 +0800

    first commit

---
+++ app/test/page.tsx
@@ -0,0 +1,9 @@
+import { title } from "@/components/primitives";
+
+export default function TestPage() {
+  return (
+    <div>
+      <h1 className={title()}>TestPage</h1>
+    </div>
+  );
+}
---
+++ config/site.ts
@@ -0,0 +1,65 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "Next.js + NextUI",
+  description: "Make beautiful websites regardless of your design experience.",
+  navItems: [
+    {
+      label: "Home",
+      href: "/",
+    },
+    // {
+    //   label: "Docs",
+    //   href: "/docs",
+    // },
+    {
+      label: "Blog",
+      href: "/blog",
+      isActive: true,
+    },
+    {
+      label: "About",
+      href: "/about",
+    },
+  ],
+  navMenuItems: [
+    {
+      label: "Profile",
+      href: "/profile",
+    },
+    {
+      label: "Dashboard",
+      href: "/dashboard",
+    },
+    {
+      label: "Projects",
+      href: "/projects",
+    },
+    {
+      label: "Team",
+      href: "/team",
+    },
+    {
+      label: "Calendar",
+      href: "/calendar",
+    },
+    {
+      label: "Settings",
+      href: "/settings",
+    },
+    {
+      label: "Help & Feedback",
+      href: "/help-feedback",
+    },
+    {
+      label: "Logout",
+      href: "/logout",
+    },
+  ],
+  links: {
+    github: "https://github.com/fh332393900",
+    twitter: "https://twitter.com/getnextui",
+    docs: "https://nextui.org",
+    sponsor: "https://www.paypal.com/paypalme/fenghangpay",
+  },
+};
